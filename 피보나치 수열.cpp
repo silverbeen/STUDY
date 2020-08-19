7c852ab2dc01a5a7e938304529fba6eeb60af447
silverbeen <kub0803@naver.com>
2020-08-19 12:08:36 +0900

    Update 피보나치 수열.cpp

--- 피보나치 수열.cpp
+++ 피보나치 수열.cpp
@@ -1,13 +1,42 @@
+//피보나치 수열 
+//앞의 2개의 숫자의 합으로 이루어진 수
+
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-	char str[4];
-	int a;
-	scanf("%s", str);
-	a=atoi(str);
-	printf("%d", a);
-} 
+	int n, fibo[100]={0,};
+	
+	fibo[0]=fibo[1]=1;
+열 
+	scanf("%d", &n);
+	
+	for(int i=2;i<n;i++){
+		fibo[i]=fibo[i-1]+fibo[i-2];
+	}
+	
+	for(int i=0;i<n;i++){
+		printf("%d ", fibo[i]);
+	}
+	
+	return 0;
+}
 
+#include <stdio.h>
+
+int main()
+{
+	int n1=1, n2=1, temp,n;
+	
+	scanf("%d", &n);
+	printf("1 1 ");
+	
+	for(int i=2;i<n;i++){
+		temp=n2;
+		n2=n1+n2;
+		printf("%d ", n2);
+		n1=temp;
+	}
+	return 0;
+}
 
