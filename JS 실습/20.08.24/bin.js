584c945d3ae69e172252c14bf7aeaa788c4ba566
강은빈 <kub0803@naver.com>
2020-08-26 17:45:25 +0900

    onclick 예제

---
+++ JS 실습/20.08.24/bin.js
@@ -0,0 +1,18 @@
+// document.getElementById("paragraph").textContent = "Hello, World";
+
+// var element = document.createElement('p');
+// element.textContent = "Hello, World";
+// document.body.appendChild(element); //add the newly created element to the DOM
+
+// window.alert(message);
+/*alert('Pause');
+console.log('alert was diminssed');*/
+
+/*if(window.confirm("Are you sure you want to delete this?")) {
+    deleteItem(itemId);
+}*/
+
+//let deleteConfirm = window.confirm("Are you sure you want to delete this?");
+
+/*let age = prompt("How old are you?");
+console.log(age); // Prints the value inserted by the user*/
