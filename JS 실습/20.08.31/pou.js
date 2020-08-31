0dd88db501962fad13288046156f4008c33d71d7
강은빈 <kub0803@naver.com>
2020-08-31 19:44:04 +0900

    JS innerHTML

---
+++ JS 실습/20.08.31/pou.js
@@ -0,0 +1,26 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+    document.getElementById("sum").innerHTML =11+5;
+    //document.write(5+10);  테스트용
+    
+    document.getElementById("demo1").innerHTML="hello, nice meet you!!";
+    document.getElementById("demo2").innerHTML="my name is eunbin";
+
+  }
+
+function mysum(){
+    let a, b,c;
+    a=4;
+    b=5;
+    c=a+b;
+    document.getElementById("test").innerHTML=c;
+}
+
+function myname(){
+    var name, nickname;
+    name = "강은빈";
+    nickname = " kelly";
+    document.getElementById("test2").innerHTML=name+nickname;
+}
+  
+  
