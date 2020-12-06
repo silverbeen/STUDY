13fb197e8eb844cab7fd4a5d5ee46c7f3d37829c
강은빈 <kub0803@naver.com>
2020-12-06 23:05:48 +0900

    todolist

--- JS 실습/TODOLIST/todo.js
+++ JS 실습/TODOLIST/todo.js
@@ -2,67 +2,79 @@ const todo = {
   input: document.querySelector("#todoinput"),
   addbutton: document.querySelector("#add"),
   list: document.querySelector("#list"),
-  listvalue: document.querySelector("#listvalue"),
+  value: document.getElementsByClassName("value"),
 };
 
-let listd = document.getElementById("listd");
-let Lchild = todo.list.children; // 자식 탐색
-
-window.onload = function () {
-  setInterval(() => {
-    for (let i = 0; i < Lchild.length; i++) {
-      Lchild[i];
-      //     console.log(Lchild[i]) // 자식을 찾는 방법
-    }
-  }, 1000);
-};
+let totalIndex = 1;
+let index = 0;
 
 /* 삭제 버튼 */
 function deleteMe(e) {
   e.currentTarget.parentNode.remove();
+  totalIndex -= 1;
 }
 
 /* 추가 버튼 클릭시 추가 */
 todo.addbutton.addEventListener("click", () => {
   console.log(12345678);
-  const todolist = ` 
-  <li class="listitem">
-              <div class="listvalue">
-                <p>${todo.input.value}</p>
-              </div>
-              <button id="modify">수정</button>
-              <!-- 삭제 버튼 -->
-              <img
-                class="itemDelete"
-                onclick="deleteMe(event);"
-                src="./close.png"
-                alt="delete"
-                title="delete"
-              />
-            </li>`;
-  // 귀찮은게 프로그래머야 이 븅진아
 
+  const todolist = ` 
+    <li class="listitem">
+      <div class="listvalue">
+        <p class="value">${todo.input.value}</p>
+      </div>
+      <button onclick="modifyBut(${
+        totalIndex - 1
+      })" class="modifyButton" id="modify">수정</button>
+      <!-- 삭제 버튼 -->
+      <img
+        class="itemDelete"
+        onclick="deleteMe(event);"
+        src="./close.png"
+        alt="delete"
+        title="delete"
+      />
+    </li>`;
   todo.list.insertAdjacentHTML("beforeend", todolist);
   todo.input.value = null;
+  totalIndex += 1;
 });
 
 const modify = {
   modal: document.querySelector(".modal"),
-  modifybutton: document.querySelector("#modify"),
+  modifybutton: document.getElementsByClassName("modifyButton"),
   complete: document.querySelector("#complete"),
   cominput: document.querySelector("#todomodify"),
 };
 
+function initialModify() {
+  [...modify.modifybutton].forEach((button, i) => {
+    console.log("됐어유");
+    button.addEventListener("click", () => {
+      test1(i);
+    });
+  });
+}
+
 /* 수정 버튼 클릭 */
-modify.modifybutton.addEventListener("click", function () {
-  console.log(12345);
+function modifyBut(idx) {
+  console.log(index, idx);
   modify.modal.style.display = "flex";
-});
+
+  index = idx;
+}
+
+/* 수정 완료 버튼 */
+function modifySucc() {
+  Array.from(todo.value).forEach((p, i) => {
+    if (i == index) {
+      p.textContent = modify.cominput.value;
+    }
+  });
+}
 
 /* 수정 완료시 모달창 닫기/수정 내용 변경 */
 modify.complete.addEventListener("click", function () {
   modify.modal.style.display = "none";
-  //modify.cominput.value = null;
-  console.log("됐다");
-  todo.listvalue.innerHTML = modify.cominput.value;
+  modify.cominput.value = null;
 });
