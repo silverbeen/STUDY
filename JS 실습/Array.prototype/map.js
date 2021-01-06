a79b38af891de77bbfe2f806bafd77c0bab75d9a
강은빈 <kub0803@naver.com>
2021-01-07 02:58:41 +0900

    map 이용해서 학생 리스트 출력

--- JS 실습/Array.prototype/map.js
+++ JS 실습/Array.prototype/map.js
@@ -8,35 +8,49 @@ let numbers = [4, 9, 16, 25, 36];
 let result = numbers.map(Math.sqrt);
 console.log(result);
 
-//이름만 출력하고 싶다
-
+//이름만 출력하고 싶
 const studentinfo = {
   studentname: document.getElementById("name"),
   studentschool: document.getElementById("school"),
   student: document.getElementById("student-info"),
-};
+  print : document.querySelector('section'),
+  search : document.getElementById('search'),
+}
 
 const students = [
   {
     school: "대덕소프트웨어 마이스터고",
     name: "silverbeen",
+    img : "https://placeimg.com/64/64/any"
   },
   {
     school: "대덕소프트웨어 마이스터고",
     name: "goldbeen",
+    img : "https://placeimg.com/64/64/any"
   },
   {
     school: "대덕소프트웨어",
     name: "jso",
+    img : "https://placeimg.com/64/64/any"
   },
 ];
 
-let schools = students.map((school) => school.school);
-let names = students.map((student) => student.name);
-console.log(names);
+//학생 정보 리스트
+students.map((c) => {
+  let name = c.name;
+  let school = c.school;
+  let img = c.img;
+  console.log(name, school);
+  
+  const info = `
+  <div id="student-info">
+      <img src="${img}" alt="프로필" />
+      <p id="name">${name}</p>
+      <p id="school">${school}</p>
+      </div>`;
+    studentinfo.print.insertAdjacentHTML('beforeend', info);
+  });
 
-for (let i of students) {
-  console.log(names, schools)
-  studentinfo.studentname.innerHTML = names[i];
-  studentinfo.studentschool.innerHTML = schools[i];
-}
+/*   //학생 검색 필터
+  let studentSerch = students.search.value; 
+  students.filter(students.name) */
