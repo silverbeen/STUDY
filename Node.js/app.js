fee101c23a324db88d03db99edfc97b6c81ba817
강은빈 <kub0803@naver.com>
2021-02-25 01:36:41 +0900

    모듈 만들어서 사용하기

--- Node.js/app.js
+++
@@ -1,9 +0,0 @@
-const calc = require("./calc.js");
-calc(2, 5); //10
-
-const calc = require("./calc.js");
-calc.geometricSum(1, 2, 3);
-
-var myMod = require("./my_module");
-myMod.foo();
-myMod.bar();
